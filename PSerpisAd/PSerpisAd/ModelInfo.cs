c1a841d27bbceb3c7221aa718c04bb8224215006
Wickfrik <carlos_coloborde@yahoo.es>
2014-02-20 12:51:27 +0100

    Upload

---
+++ PSerpisAd/PSerpisAd/ModelInfo.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reflection;
+
+namespace Serpis.Ad
+{
+	public class ModelInfo
+	{
+		private Type type;
+		public ModelInfo ()
+		{
+			this.type = type;
+			
+			public static void GetSelect(Type type) {
+				string keyName = null;
+				
+				List<string> fieldNames = new List<string>();
+				foreach (PropertyInfo propertyInfo in type.GetProperties ()) {
+					if (propertyInfo.IsDefined (typeof(KeyAttribute), true))
+						keyName = propertyInfo.Name.ToLower ();
+					else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
+						fieldNames.Add (propertyInfo.Name.ToLower());
+				}
+
+			tableName = type.Name.ToLower();
+		}
+		
+		private string tableName;
+
+		public string UpdateText{
+			get { return; }
+		
+	}
+}
+
